a8b9f4a58cc5676bfd2d291a3a92ea045e6cd46e
Alexis Pain <apain@excellium-services.lu>
2020-09-25 13:03:07 +0200

    Adding server_type checks for analysisData

--- common/analysis_data.py
+++ common/analysis_data.py
@@ -1,3 +1,6 @@
+from common.server_type import ServerType
+
+
 class AnalysisData:
     """Represent the information resulting of the analysis."""
 
@@ -8,6 +11,8 @@ class AnalysisData:
         :param issue_datas: The list of issue_data objects that reprents the found issues.
         :param config_file_name: The name of the file from which the configuration is issued.
         """
+        if not isinstance(server_type, ServerType):
+            raise Exception("The server type must be a item of the ServerType enumeration!")
         self.server_type = server_type
         self.issue_datas = issue_datas
         self.config_file_name = config_file_name
