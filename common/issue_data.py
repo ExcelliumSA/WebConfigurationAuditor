eed176f8b580f70d811fc47fe5d95d44895b279f
Alexis Pain <apain@excellium-services.lu>
2020-09-25 11:46:25 +0200

    Adding the issue_data class

---
+++ common/issue_data.py
@@ -0,0 +1,13 @@
+class issue_data:
+    """Represent the information of a found issue."""
+
+    def __init__(self, details, rule_id, CIS_version):
+        """Constructor.
+
+        :param details: A short description of the issue.
+        :param rule_id: The ID of the rule from the CIS.
+        :param CIS_version: The version of CIS the rule ID refers to.
+        """
+        self.details = details
+        self.rule_id = rule_id
+        self.CIS_version = CIS_version
