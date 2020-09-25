95401fbba8f94e3c95874bf2d29c9524b28e360f
Alexis Pain <apain@excellium-services.lu>
2020-09-25 16:27:37 +0200

    Adding Apache config file parser

--- parsing/parser.py
+++ parsing/parser.py
@@ -23,3 +23,19 @@ def parse_audit_rules(rule_file_name):
         rule = AuditRule.unserializer(json_rule)
         rules_list.append(rule)
     return rules_list
+
+
+def parse_config_data_apache(config_file_name):
+    """Parse an Apache config file and return its content without the comments.
+
+    :param config_file_name: The name of the config file to parse.
+    :return: A string only containing the active configuration of the Apache.
+    """
+    with open(config_file_name) as config_file:
+        lines = config_file.readlines()
+    content = ""
+    for line in lines:
+        if line[:2] == "# ":
+            continue
+        content += line + '\n'
+    return content
