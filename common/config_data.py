eef4e5f1807479b870a65a7d7ea74b9b9527c186
drighetto <drighetto@excellium-services.com>
2020-09-25 13:49:00 +0200

    Add validation

--- common/config_data.py
+++ common/config_data.py
@@ -12,9 +12,15 @@ class ConfigData:
         :param config_file_name: The name of the file from which the configuration is issued.
         :param audit_rules: The list of audit rules that must be applied against the configuration.
         """
+        if not isinstance(server_type, ServerType):
+            raise Exception("The server type must be a item of the ServerType enumeration!")
+        if config_content is None or len(config_content.strip(" ")) == 0:
+            raise Exception("Configuration cannot be none or empty!")
+        if config_file_name is None or len(config_file_name.strip(" ")) == 0:
+            raise Exception("Configuration file name cannot be none or empty!")
+        if audit_rules is None or len(audit_rules) == 0:
+            raise Exception("Audit rules cannot be none or empty!")
         self.server_type = server_type
         self.config_content = config_content
         self.config_file_name = config_file_name
         self.audit_rules = audit_rules
-        if not isinstance(server_type, ServerType):
-            raise Exception("The server type must be a item of the ServerType enumeration!")
