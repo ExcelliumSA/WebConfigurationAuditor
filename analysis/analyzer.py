b58cb6fab1552ee743543688dc52acb815c08efd
drighetto <drighetto@excellium-services.com>
2020-10-02 09:57:20 +0200

    Update issue message to made them more easy to understand.

--- analysis/analyzer.py
+++ analysis/analyzer.py
@@ -21,7 +21,7 @@ def analyze(config_data_collection):
     # Apply processing
     analysis_results = []
     issue_msg_template_matched = "Rule '%s' has found the following insecure configuration: %s."
-    issue_msg_template_not_matched = "Rule '%s' has not found the expected recommended secure confguration."
+    issue_msg_template_not_matched = "Rule '%s' has not found the expected recommended secure configuration."
     debug_msg_template = "Test rule '%s' with the regex '%s' has %s matched."
     current_rule_identifier = ""
     current_regex = ""
