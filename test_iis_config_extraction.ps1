dca348cd6099cbc3d65ceb9d27e81dd40a862d3d
drighetto <drighetto@excellium-services.com>
2020-12-24 14:15:44 +0100

    Add CI test for #15

--- test_iis_config_extraction.ps1
+++ test_iis_config_extraction.ps1
@@ -6,16 +6,14 @@
 # and IIS roles are installed by default on runnners
 $iisScript = ".\references\export-iis-config.ps1"
 $filename = "$env:computername-IIS.json"
-$outFile = ".\out.txt"
 # Display the web roles installation state
 Get-WindowsFeature | Where-Object {($_.name -eq "Web-Server") -or ($_.name -eq "Web-WebServer")}
 # CASE 1
 # IIS roles are installed
 & $iisScript
 $count = (Get-Content $filename | Select-String -SimpleMatch -Pattern '"InternalFunctionsInError":[]').length
-Write-Host "Found: $count"
 if ($count -ne 1){
-    Write-Host "Some internal functions meet an errors!"
+    Write-Host "Some internal functions raise a error!"
     $content = Get-Content $filename
     Write-Host "JSON:"
     Write-Host $content
