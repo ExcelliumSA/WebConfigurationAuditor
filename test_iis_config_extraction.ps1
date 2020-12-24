51a0890bf64b7c6b526352378ec1303bde35a3df
drighetto <drighetto@excellium-services.com>
2020-12-24 14:04:51 +0100

    Add CI test for #15

--- test_iis_config_extraction.ps1
+++ test_iis_config_extraction.ps1
@@ -1,31 +1,24 @@
 #############################################################
 ## Test the IIS config extraction script in different cases
 #############################################################
+# NOTE:
+# Case where IIS roles are not installed cannot be tested because roles cannot be removed 
+# and IIS roles are installed by default on runnners
 $iisScript = ".\references\export-iis-config.ps1"
 $filename = "$env:computername-IIS.json"
 $outFile = ".\out.txt"
 # Display the web roles installation state
 Get-WindowsFeature | Where-Object {($_.name -eq "Web-Server") -or ($_.name -eq "Web-WebServer")}
 # CASE 1
-Uninstall-WindowsFeature -name Web-Server -IncludeManagementTools
-# IIS roles are not installed
-& $iisScript | Out-File -FilePath $outFile
-$count = (Get-Content $outFile | Select-String -Pattern 'are not installed, extraction cancelled!').length
-if ($count -ne 1){
-    Write-Host "Output of the script is not the one expected:"
-    $content = Get-Content $outFile
-    Write-Host $content
-    Exit 1
-}
-# CASE 2
 # IIS roles are installed
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 & $iisScript
 $count = (Get-Content $filename | Select-String -Pattern '"InternalFunctionsInError": []').length
+Write-Host "Found: $count"
 if ($count -ne 1){
     Write-Host "Some internal functions meet an errors!"
     $content = Get-Content $filename
     Write-Host "JSON:"
     Write-Host $content
-    Exit 2
+    Exit 1
 }
