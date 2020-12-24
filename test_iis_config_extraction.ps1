e52feb466f48e7df8d3a577392be754cf325ff85
drighetto <drighetto@excellium-services.com>
2020-12-24 13:56:19 +0100

    Add CI test for #15

--- test_iis_config_extraction.ps1
+++ test_iis_config_extraction.ps1
@@ -20,16 +20,11 @@ if ($count -ne 1){
 # IIS roles are installed
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 & $iisScript
-if ($LASTEXITCODE -ne 0){
-    Write-Host "Return code 0 was expected but $LASTEXITCODE was obtained!"
-    Exit 2
-}
 $count = (Get-Content $filename | Select-String -Pattern '"InternalFunctionsInError": []').length
 if ($count -ne 1){
     Write-Host "Some internal functions meet an errors!"
     $content = Get-Content $filename
     Write-Host "JSON:"
     Write-Host $content
-    Exit 3
+    Exit 2
 }
-Exit 0
