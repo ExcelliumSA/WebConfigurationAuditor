34e5a31020045c87247c5ca67781ede58291cd4c
drighetto <drighetto@excellium-services.com>
2020-12-29 18:23:58 +0100

    Repair build error

---
+++ test_iis_config_extraction.ps1
@@ -0,0 +1,23 @@
+#############################################################
+## Test the IIS config extraction script in different cases
+#############################################################
+# NOTE:
+# Case where IIS roles are not installed cannot be tested because roles cannot be removed 
+# and IIS roles are installed by default on runnners
+$iisScript = ".\references\export-iis-config.ps1"
+$filename = "$env:computername-IIS.json"
+# Display the web roles installation state
+Get-WindowsFeature | Where-Object {($_.name -eq "Web-Server") -or ($_.name -eq "Web-WebServer")}
+# Display IIS version
+Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\InetStp\' | select setupstring
+# CASE 1
+# IIS roles are installed
+& $iisScript
+$count = (Get-Content $filename | Select-String -SimpleMatch -Pattern '"InternalFunctionsInError":[]').length
+if ($count -ne 1){
+    Write-Host "Some internal functions raise a error!"
+    $content = Get-Content $filename
+    Write-Host "JSON:"
+    Write-Host $content
+    Exit 1
+}
